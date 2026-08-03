5c7832ba563de3f7301a428771f04c127d7c5c19
erogramer <sentit.kr@gmail.com>
2026-08-03 16:35:27 +0900

    PC 헤더 메뉴를 클릭 토글에서 호버 방식으로 복구, 다중 열림 버그 수정
    - PC 상단 헤더에서는 화살표 토글 버튼을 숨기고, 기존 :before 화살표 아이콘과
      함께 마우스 호버로 드롭다운이 열리는 원래 방식으로 복구 (assets/css/menu.css)
    - 모바일 오프캔버스/사이드 메뉴는 호버가 안 되므로 토글 버튼 클릭 방식을 유지
    - 토글 버튼 클릭 시 같은 depth의 다른 열린 메뉴를 닫지 않아 클릭한 메뉴가 계속
      쌓이던 버그 수정 (조상 메뉴는 유지하고 형제 메뉴만 닫음, assets/js/menu.js)

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -13,16 +13,24 @@ $(function() {
 		});
 
 
-		// 상단메뉴(PC)/오프캔버스메뉴(모바일) 공통 : 1depth 는 기본노출, 하위 depth 는
-		// .menu-toggle 버튼 클릭으로 여닫는 아코디언 UI. 마크업은 render_menu_dropdown() /
-		// render_menu_accordion() 헬퍼(application/helpers/basic_helper.php)에서 출력되며
-		// .js-clone-nav 를 clone 할 때 .menu-toggle 버튼도 함께 복제됩니다.
+		// 오프캔버스메뉴(모바일) : 1depth 는 기본노출, 하위 depth 는 .menu-toggle 버튼
+		// 클릭으로 여닫는 아코디언 UI. PC 상단메뉴는 호버로 열리므로 이 버튼은 숨겨져
+		// 있습니다(assets/css/menu.css). 마크업은 render_menu_dropdown() 헬퍼
+		// (application/helpers/basic_helper.php)에서 출력되며 .js-clone-nav 를
+		// clone 할 때 .menu-toggle 버튼도 함께 복제됩니다.
 		$('body').on('click', '.site-menu .menu-toggle, .site-nav-wrap .menu-toggle', function(e) {
 			e.preventDefault();
 			e.stopPropagation();
 
 			var $li = $(this).closest('li.has-children');
 			var isOpen = $li.hasClass('open');
+			var $siblingWrap = $li.parent();
+
+			// 같은 depth의 다른 메뉴가 열려있다면 먼저 닫아, 클릭한 메뉴만 열리도록 합니다
+			// (조상 메뉴는 현재 메뉴가 보이기 위해 열려 있어야 하므로 건드리지 않습니다)
+			$siblingWrap.children('li.has-children.open').not($li).each(function() {
+				$(this).removeClass('open').find('> .menu-toggle').attr('aria-expanded', 'false');
+			});
 
 			$li.toggleClass('open', ! isOpen);
 			$(this).attr('aria-expanded', ! isOpen ? 'true' : 'false');
